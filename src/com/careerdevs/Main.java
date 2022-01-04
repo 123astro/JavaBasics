f81e2da3c20737d5d0c1f9f1b486e0be9fad9531
Keith Benjamin <kjb528i@yahoo.com>
2022-01-03 20:30:03 -0500

    assigned variables values

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -1,14 +1,10 @@
 package com.careerdevs;
 
-import java.sql.SQLOutput;
-
 public class Main {
 
     public static void main(String[] args) {
-	String firstName = "Keith";
-    String lastName = "Benjamin";
-
+        String firstName = "Keith";
+        String lastName = "Benjamin";
         System.out.println(firstName + " " + lastName);
-
     }
 }
