f39c4774b3159c6cbd36302d6139c92ef009224e
Keith Benjamin <kjb528i@yahoo.com>
2022-01-03 21:13:44 -0500

    assigned variables values

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -5,6 +5,11 @@ public class Main {
     public static void main(String[] args) {
         String firstName = "Keith";
         String lastName = "Benjamin";
+        createFullName(firstName, lastName);
+    }
+
+    public static void createFullName(String firstName, String lastName) {
         System.out.println(firstName + " " + lastName);
     }
+
 }
