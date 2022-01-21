cb283b6cac300b871272b17798023fb8349f2e21
Keith Benjamin <kjb528i@yahoo.com>
2022-01-20 19:34:29 -0500

    added UserInterface

---
+++ src/com/careerdevs/asktheuser/UserInterface.java
@@ -0,0 +1,50 @@
+package com.careerdevs.asktheuser;
+
+import java.util.Scanner;
+
+public class UserInterface {
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        System.out.println("What type of car do you have?");
+        String carType = scanner.nextLine();
+        System.out.println("I have a " + carType + ".\n");
+
+        System.out.println("How many cylinders does your " + carType + " have ?");
+        byte cylinders = scanner.nextByte();
+        System.out.println("My " + carType + " has " + cylinders + " cylinders.\n");
+
+        System.out.println("The " + carType + "'s" + " speedometer goes to what speed?");
+        short speedometer = scanner.nextShort();
+        System.out.println("The " + carType + " has a speedometer that reads " + speedometer + "\n");
+
+        System.out.println("How many mile warranty is on the " + carType);
+        int mileWarranty = scanner.nextInt();
+        System.out.println("The " + carType + " has a " + mileWarranty + " mile warranty.\n");
+
+        System.out.println("How much did the " + carType + " cost?");
+        float carCost = scanner.nextFloat();
+        System.out.println("The " + carType + " cost : " + carCost + "\n");
+
+        System.out.println("How many vehicles has " + carType + " sold since the start of the company?");
+        long carsSold = scanner.nextLong();
+        System.out.println(carType + " has sold " + carsSold + " vehicles lifetime.\n");
+
+        System.out.println("What is the cubic capacity of the " + carType + "?");
+        double cubicCap = scanner.nextDouble();
+        System.out.println("The " + carType + " has a cubic capacity of " + cubicCap + "\n");
+
+        System.out.println("Do you believe the " + carType + " is a good buy?");
+        boolean goodBuy = scanner.nextBoolean();
+        System.out.println("The " + carType + " is a good buy ? " + goodBuy + "\n");
+
+
+    }
+
+    public static void carType() {
+        System.out.println("What type of car do you have?");
+        String carType = scanner.nextLine();
+        System.out.println("I have a " + carType + ".\n");
+    }
+}
--- src/com/careerdevs/sayhello/Main.java
+++ src/com/careerdevs/sayhello/Main.java
@@ -2,29 +2,76 @@ package com.careerdevs.sayhello;
 
 import com.careerdevs.NameGenerator;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
+
+    public static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        whatIsYourName();
-        whatIsYourFullName();
-
-
+        whatIsYourName("What is your first name");
+        whatIsYourFullName("First prompt type your first name. Second prompt, type your last name");
+        simpleCalculator();
     }
 
-    public static void whatIsYourName(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("What is your name? ");
-            String name = scanner.next();
-        System.out.println("Hello, nice to meet you " + name + ".");
+    public static void whatIsYourName(String question) {
+        String userName = getUserInput(question);
+        System.out.println("Hello, nice to meet you " + userName + ".\n");
     }
 
-    public static void whatIsYourFullName(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("What is your first name? ");
-        String firstName = scanner.next();
-        System.out.println("What is your last name? ");
-        String lastName = scanner.next();
+    public static void whatIsYourFullName(String question) {
+        String firstName = getUserInput(question);
+        String lastName = getUserInput(question = "");
         NameGenerator.generateFullName(firstName, lastName);
     }
+
+    public static void userSignUp() {
+        String userName = getUserInput("Enter a username"); // invoking getUserInput
+        String passWord = getUserInput("Enter a password");
+        System.out.println("Thanks for signing up " + userName);
+        if (passWord.length() < 5) {
+            System.out.println("Your password sucks!");
+        }
+    }
+
+    public static String getUserInput(String question) {
+        System.out.print(question + "\nInput: ");
+        String input = scanner.nextLine();
+        return input;
+    }
+
+    public static int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static void simpleCalculator() {
+        int inputNum1 = getUserIntInput("Enter first number to add");
+        int inputNum2 = getUserIntInput("Enter second number to add");
+
+        int sumTotal = sum(inputNum1, inputNum2);
+
+        System.out.println("The sum of " + inputNum1 + " and " + inputNum2 + " is " + sumTotal + "\n");
+    }
+
+    public static int getUserIntInput(String question) {
+        System.out.println((question + "\nInt: "));
+        int input = 0;
+//        while (true) {
+//            try{
+//                input = scanner.nextInt();
+//                break;
+//            }
+//            catch (InputMismatchException e) {
+//                System.out.println("Please enter a valid number");
+//                scanner.nextLine();
+//            }
+//        }
+        return input;
+//        while (!scanner.hasNextInt()) {
+//            System.out.println("Please enter a number");
+//            scanner.nextInt();
+//        }
+    }
 }
+
