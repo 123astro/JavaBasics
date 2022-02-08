35e655d274d44a10b91d37be6a371dc5c8e3c2d1
Keith Benjamin <kjb528i@yahoo.com>
2022-02-07 22:03:37 -0500

    created a method that counts down to 0 from the passed parameter.

--- src/com/careerdevs/introtorecursion/Recursion.java
+++ src/com/careerdevs/introtorecursion/Recursion.java
@@ -2,13 +2,21 @@ package com.careerdevs.introtorecursion;
 
 public class Recursion {
     /*
-    Recursion is a method that calls itself and always needs a way to break out of the loop of calling itself.
+    Recursion is a method that calls itself and always needs a way to break out of the loop of calling itself
+    otherwise it will become an infinite loop.
 
 
      */
 
     public static void main(String[] args) {
-
+        countDown(10);
     }
 
-}
+    public static void countDown(int num){
+
+     for(int i = num; i >=0; i--)
+           System.out.println(i);
+       }
+    }
+
+
